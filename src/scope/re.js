2a90c71758b32184ec2050605ebfc1b485fb7fbf
Daniel Alejandro <yidenlarren@gmail.com>
2022-10-26 21:11:41 -0500

    reasignación-de-Valores

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName; // Undefined / declaramos
+firstName = ('Oscar') // asignamos 
+console.log(firstName);
+
+var lastName = ('David'); // asignamos 7 declaramos
+lastName = 'Ana'; // reasignamos
+console.log(lastName)
+
+var secondName = ('David'); // declarar / asignar
+var secondName = 'Ana'; // reasignamos
+console.log(secondName);
+
+// Let
+let fruit = ('apple'); // declarar / asignar
+fruit = 'Kiwi'; // reasignamos
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// const 
+const animal = ('Dog'); // declarar 7 asignar
+// animal = 'Gato'; // reasignando
+const animal = 'snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('🚗');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
